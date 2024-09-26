31cf9672d764a2224afa1ae6d4da9e82c1e3e8e5
xiaotan <2653591683@qq.com>
2024-09-26 14:10:22 +0800

    9.24

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let pendingRequest = [];
+const pendingRequest = new Map();
 
 const service = axios.create({
     baseURL: 'http://0.0.0.0:4001',
@@ -10,30 +10,39 @@ const service = axios.create({
     },
 });
 
+const getRequestKey = (config) => {
+    return [config.method, config.url].join(':');
+};
+
 service.interceptors.request.use(
     (config) => {
+        const requestKey = getRequestKey(config);
+        if (pendingRequest.has(requestKey)) {
+            const cancelToken = pendingRequest.get(requestKey);
+            cancelToken.cancel('Cancel duplicate request');
+            pendingRequest.delete(requestKey);
+        }
+        const cancelToken = axios.CancelToken.source();
+        config.cancelToken = cancelToken.token;
+        pendingRequest.set(requestKey, cancelToken);
         return config;
     },
-    (error) => {
-        return Promise.reject(error);
+    (error) =>{
+        return Promise.reject(error)
     }
 );
 
-function cancelRepeatRequest(config) {
-    for (let i = 0; i < pendingRequest.length; i++) {
-        if (pendingRequest[i].url === config.url) {
-            pendingRequest[i].cancel();
-            pendingRequest.splice(i, 1);
-        }
+service.interceptors.response.use(
+    (response) => {
+        const requestKey = getRequestKey(response.config);
+        pendingRequest.delete(requestKey);
+        return response;
+    },
+    (error) => {
+        const requestKey = getRequestKey(error.config || {});
+        pendingRequest.delete(requestKey);
+        return Promise.reject(error);
     }
-}
-
-function removePendingRequest(config) {
-    for (let i = 0; i < pendingRequest.length; i++) {
-        if (pendingRequest[i].url === config.url) {
-            pendingRequest.splice(i, 1);
-        }
-    }
-}
+);
 
 export default service;
--- src/utils/model.js
+++ src/utils/model.js
@@ -1,6 +1,6 @@
 import service from "./axios.js";
 
-const get = (url, params) => service.get(url, { params });
+const get = (url, params) => service.get(url, params);
 const post = (url, data) => service.post(url, data);
 const put = (url, data) => service.put(url, data);
 const del = (url) => service.delete(url);
